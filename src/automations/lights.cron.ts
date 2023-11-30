3f1d21105f5a6398082b02df64244a7dcf7ff68e
Sandoche <sandoche@protonmail.com>
2023-11-30 09:12:17 +0000

    Adding new cron jobs

--- src/automations/lights.cron.ts
+++ src/automations/lights.cron.ts
@@ -17,9 +17,27 @@ export class LightsCron {
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_8PM)
-  async afternoonCron() {
+  async afternoonCronFirst() {
+    this.logger.debug('Dimming lights');
+    await this.lightsService.update(50, 2000, '#FE8714');
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_9PM)
+  async afternoonCronSecond() {
+    this.logger.debug('Dimming lights');
+    await this.lightsService.update(40, 1750, '#FE8714');
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_10PM)
+  async afternoonCronThird() {
     this.logger.debug('Dimming lights');
     await this.lightsService.update(30, 1500, '#FE8714');
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_11PM)
+  async afternoonCronForth() {
+    this.logger.debug('Dimming lights');
+    await this.lightsService.update(20, 1000, '#FE8714');
   }
 
   @Cron(CronExpression.EVERY_10_MINUTES)
